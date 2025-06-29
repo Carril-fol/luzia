1fbb2b9b170aff205d03a7f0af5e4a4ff680bcac
Folco <folco.carril@gmail.com>
2025-06-28 22:11:06 -0300

    feat: User models added to the project

---
+++ backend/models/user_model.py
@@ -0,0 +1,36 @@
+from datetime import time, datetime
+from typing import Optional
+from pydantic import BaseModel, Field, field_validator, EmailStr, model_validator
+
+class UserModel(BaseModel):
+    id: Optional[int] = Field(default=None, description="ID of the user")
+    username: str = Field(default=None, description="Username from the user")
+    first_name: str
+    last_name: str
+    email: EmailStr
+    last_login: time
+
+
+class CreateUserModel(BaseModel):
+    username: str = Field(default=None, description="Username from the user")
+    first_name: str
+    last_name: str
+    email: EmailStr
+    password: str
+    confirm_password: str
+    date_register: Optional[datetime] = Field(default=datetime.now())
+    last_login: Optional[datetime] = Field(default=datetime.now())
+
+    @field_validator("password")
+    def validate_password(cls, value):
+        if len(value) < 8:
+            raise ValueError('Password must be at least 8 characters long')
+        if not any(character.isupper() for character in value):
+            raise ValueError('Password must contain at least one uppercase letter')
+        return value
+
+    @model_validator(mode="after")
+    def password_match(self):
+        if self.password != self.confirm_password:
+            raise ValueError("Passwords do not match")
+        return self
