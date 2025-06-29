564e6e7cf1ac5a8bb6bb9c0f1881846baba81ab3
Folco <folco.carril@gmail.com>
2025-06-29 17:07:04 -0300

    feat: Functionalities added in service (user) for they use

--- backend/services/user_services.py
+++ backend/services/user_services.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from .base_service import BaseService
 from entities.user_entity import UserEntity
 from repositories.user_repository import UserRepository
-from models.user_model import CreateUserModel
+from models.user_model import CreateUserModel, AuthenticateUserModel, UserModel
 
 
 class UserServices(BaseService):
@@ -11,10 +11,37 @@ class UserServices(BaseService):
     def __init__(self, user_repository: UserRepository):
         self._user_repository = user_repository
 
-    def create_user(self, data: dict):
-        validated_data = self._prepare_to_entity(data, CreateUserModel).model_dump()
-        validated_data["password"] = generate_password_hash(data["password"])
-        validated_data.pop("confirm_password", None)
+    def get_user_by_email(self, data: dict):
+        user = self._user_repository.get_user_by_email(data["email"])
+        if not user:
+            raise Exception("User not exists")
+        
+        return self._validate_entity_and_serialize(user, UserModel)
+    
+    def get_user_by_id(self, id):
+        user = self._user_repository.get_user_by_id(UserEntity, id)
+        if not user:
+            raise Exception("User not exists")
+        
+        return self._validate_entity_and_serialize(user, UserModel)
 
-        user_to_create = UserEntity(**validated_data)
+    def authenticate_user(self, data: dict):
+        auth_data = AuthenticateUserModel(**data)
+
+        user = self._user_repository.get_user_by_email(data["email"])
+        if not user:
+            raise Exception("User not exists")
+        
+        if not check_password_hash(user.password, auth_data.password):
+            raise Exception("Password not match")
+        
+        return user
+
+    def create_user(self, data: dict):
+        register_data = CreateUserModel(**data)
+        hashed_password = generate_password_hash(register_data.password)
+        register_data.password = hashed_password
+
+        register_data_dict = register_data.model_dump(exclude={"confirm_password"})
+        user_to_create = UserEntity(**register_data_dict)
         return self._user_repository.create_user(user_to_create)
