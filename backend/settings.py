b8a47035e3d51668a26f12bc667cf6377aa37688
Folco <folco.carril@gmail.com>
2025-06-29 21:00:54 -0300

    refactor: Database URL added in config

--- backend/settings.py
+++ backend/settings.py
@@ -4,15 +4,15 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    DATABASE_API_KEY = os.getenv("DATABASE_API_KEY")
-    SERVER_HOST = os.getenv("SERVER_HOST")
-    SERVER_PORT = int(os.getenv("SERVER_PORT", 8000))
+    DB_USER = os.getenv("MYSQL_USER")
+    DB_PASS = os.getenv("MYSQL_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_NAME = os.getenv("MYSQL_DATABASE")
+    DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:3306/{DB_NAME}"
     JWT_TOKEN_LOCATION = ["cookies"]
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ACCESS_TOKEN_EXPIRES = 1800
     JWT_REFRESH_TOKEN_EXPIRES = 2592000
-    CORS_SUPPORTS_CREDENTIALS = True
 
 
 class DevelopmentConfig(Config):
